6574174c359b066e7b51784d58d4f0a50ca7a422
orloffv <orloff.v@gmail.com>
2012-03-16 12:05:01 +0400

    Added sms & icons

---
+++ js/me.js
@@ -0,0 +1,19 @@
+$(function () {
+    var $jurl = $.jurlp();
+    $("[data-sms]").click(function()
+    {
+        if ($jurl != 'orloffv.ru')
+        {
+            $jurl.url("http://orloffv.ru/sms/send");
+            $jurl.goto();
+        }
+        else
+        {
+            $.get('sms/popup', function(data)
+            {
+                $('#modal-content', '#smsModal').html(data);
+                $('#smsModal').modal();
+            });
+        }
+    });
+});
